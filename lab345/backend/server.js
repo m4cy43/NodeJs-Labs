4c6a3e23774629a857515f1364eb74fe663ef0a1
Andrii Konovalchuk <andrii.konovalchuk.work@gmail.com>
2023-04-05 20:37:47 +0300

    lab345. server frame

---
+++ lab345/backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const dotenv = require("dotenv").config({
+  path: "../.env",
+});
+const { errorHandler } = require("./middleware/errorMiddleware");
+const dbConnect = require("./config/db");
+const port = process.env.PORT || 5000;
+const app = express();
+
+// MongoDB connection
+dbConnect();
+
+// Request body parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// ALL ROUTES
+// TODO
+
+// LAST IN MIDDLEWARE!
+// Error middleware
+app.use(errorHandler);
+
+// Port listening
+app.listen(port, () => console.log(`Server started on port ${port}`));
